a1affb58280025a99832543ae79dcc11715067f1
klubben <tosha-bed@yandex.ru>
2025-04-02 21:31:35 +0300

    Setup package building

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
+/// <reference types="vitest" />
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import glsl from "vite-plugin-glsl";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), glsl()],
+  test: {
+    environment: "jsdom",
+    environmentOptions: {
+      jsdom: {
+        pretendToBeVisual: true,
+        resources: "usable",
+        console: true,
+      },
+    },
+    fakeTimers: {
+      shouldAdvanceTime: true,
+    },
+    globals: true,
+    include: ["**/*.test.ts"],
+    setupFiles: ["vitest-setup.ts"],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+  },
+  plugins: [
+    tsconfigPaths(),
+    glsl(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
 });
